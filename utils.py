0a719b793e8b0394daa8f6a5f978d727d6b6d0a9
Rostislav62 <33193719+Rostislav62@users.noreply.github.com>
2024-05-23 21:48:08 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,19 +6,19 @@ from config import keys
 class ConvertionException(Exception):
     pass
 
+
 class CryptoConverter:
     @staticmethod
     def convert(quote: str, base: str, amount: str):
 
-        
         if quote == base:
             raise ConvertionException('Unable to transfer identical currencies {base}.')
-        
+
         try:
             quote_ticker = keys[quote]
         except KeyError:
             raise ConvertionException('We were unable to process {quote} currency.')
-        
+
         try:
             base_ticker = keys[base]
         except KeyError:
@@ -28,8 +28,7 @@ class CryptoConverter:
             amount = float(amount)
         except ValueError:
             raise ConvertionException('We were unable to process the quantity {amount}.')
-        
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym{quote_ticker}&tsym{base_ticker}')
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]]
 
         return total_base
